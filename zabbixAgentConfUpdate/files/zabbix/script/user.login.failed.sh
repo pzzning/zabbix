8efa07c30ba75647b3fdeedb51e7b256dca93b93
pzzning <969083154@qq.com>
2019-01-11 10:32:40 +0800

    update zabbix20190111

---
+++ zabbixAgentConfUpdate/files/zabbix/script/user.login.failed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#zhengning
+#20190108
+
+# user login failed
+
+/usr/bin/lastb |wc -l
+
+
---
+++ zabbixAgentConfUpdate/files/zabbix/script/user.login.success.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#zhengning
+#20190108
+
+# user login success
+
+/usr/bin/last |grep $1 |wc -l
